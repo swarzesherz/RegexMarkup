06f72ce15168ddd2139c4adae18717a39822aee5
Herz <achwazer@gmail.com>
2011-09-30 10:32:51 -0500

    Start migrating to VSTO project in C#

---
+++ RegexMarkup/RegexMarkup/Main.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Tools.Applications.Runtime;
+using Word = Microsoft.Office.Interop.Word;
+using Office = Microsoft.Office.Core;
+
+namespace RegexMarkup
+{
+    public partial class ThisAddIn
+    {
+        Office.CommandBar commandBarMarkup;
+        Office.CommandBarButton regexButton;
+
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+            AddToolbar();
+        }
+        private void AddToolbar()
+        {
+            try
+            {
+                commandBarMarkup = Application.CommandBars["Markup SciELO Mexico"];
+            }
+            catch (ArgumentException e)
+            {
+                // Toolbar named Test does not exist so we should create it.
+            }
+
+            if (commandBarMarkup == null)
+            {
+                // Add a commandbar named Test.	
+                commandBarMarkup = Application.CommandBars.Add("Markup Scielo Mexico", 1, missing, true);
+            }
+
+            try
+            {
+                // Add a button to the command bar and an event handler.
+                regexButton = (Office.CommandBarButton)commandBarMarkup.Controls.Add(
+                    1, missing, missing, missing, missing);
+
+                regexButton.Style = Office.MsoButtonStyle.msoButtonCaption;
+                regexButton.Caption = "Markup Regex";
+                regexButton.Tag = "Markup Regex";
+                regexButton.Click += new Office._CommandBarButtonEvents_ClickEventHandler(ButtonClick);
+
+                commandBarMarkup.Visible = true;
+            }
+            catch (ArgumentException e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        // Handles the event when a button on the new toolbar is clicked.
+        private void ButtonClick(Office.CommandBarButton ctrl, ref bool cancel)
+        {
+            MessageBox.Show("You clicked: " + ctrl.Caption);
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region Código generado de VSTO
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion
+    }
+}
---
+++ RegexMarkup/RegexMarkup/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos atributos para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("RegexMarkup")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("RegexMarkup")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible como false hace que los tipos de este ensamblado no sean visibles 
+// a los componentes COM. Si necesita obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible como true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como identificador de la biblioteca de tipos si este proyecto se expone a COM
+[assembly: Guid("ce9c071a-8d0f-4669-9fc6-229c86a63a59")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria 
+//      Número de versión de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
